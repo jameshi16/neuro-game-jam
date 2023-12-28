82f0a50f459a7a51210d3b1b1710e09cd48e7f7c
James <lim.en.hui.james@gmail.com>
2023-12-28 23:24:25 +0000

    Initial Commit

---
+++ player.gd
@@ -0,0 +1,38 @@
+extends Node2D
+signal hit
+
+# add a variable that adjusts speed
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(_body: Node2D):
+	# TODO: Probably should flash because damaged
+	hit.emit()
