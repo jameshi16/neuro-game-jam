9f568977cdb5a4048c1bdd6709e45f10c17ee927
James <lim.en.hui.james@gmail.com>
2023-12-29 08:07:37 +0000

    World zoom when level starts

--- main.gd
+++ main.gd
@@ -10,14 +10,37 @@ var screen_size
 # Game variables
 var score = 0
 
+# TODO: maybe need state manager? but it's a jam, so I'll just leave a note
+
+
 func ready_up_camera():
-	# Sets the camera view to the whole world, then sets the current viewpoint camera
 	$Camera2D.set_position(Vector2(0, 0))
-	$Camera2D.set_zoom(Vector2(
+	var larger_scale = max(
 		screen_size.x / float(tilemap_scale.x * tilemap_size.x),
-		screen_size.y / float(tilemap_scale.y * tilemap_size.y)))
-	# $Camera2D.set_limit(Rect2(0, 0, $TileMap.get_used_rect().size.x, $TileMap.get_used_rect().size.y))
+		screen_size.y / float(tilemap_scale.y * tilemap_size.y)
+	)
+	$Camera2D.set_zoom(Vector2(larger_scale, larger_scale))
+	#$Camera2D.set_limit(
+	#	Rect2(0, 0, $TileMap.get_used_rect().size.x, $TileMap.get_used_rect().size.y))
 	$Camera2D.make_current()
+
+
+func reset_camera_view():
+	$Player.keys_disabled = false
+	$Player.reset_camera_view()
+
+
+func pan_entire_world():
+	# sets up a one shot timer, allow the user to interact with the camera to look around for 5 seconds
+	$Player.keys_disabled = true
+	var timer = Timer.new()
+	add_child(timer)
+
+	timer.wait_time = 5.0
+	timer.one_shot = true
+	timer.start()
+	ready_up_camera()
+	timer.timeout.connect(reset_camera_view)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -28,28 +51,32 @@ func _ready():
 	print(screen_size)
 	tilemap_size = $TileMap.get_used_rect().size
 
-	# calculates the full scale. there is probably a correct way to do tiles, but like clueless
+	# calculates the full scale. there is probably a correct way to do tiles, but i'm dumb
 	tilemap_scale = $TileMap.tile_set.tile_size
 	tilemap_scale.x *= $TileMap.scale.x
 	tilemap_scale.y *= $TileMap.scale.y
-	for n in 10:
-		var item_position = Vector2(
-			randf_range(0, tilemap_size.x * tilemap_scale.x),
-			randf_range(0, tilemap_size.y * tilemap_scale.y)
-			)
-		var item_instance = item_scene.instantiate()
-		item_instance.position = item_position
-		add_child(item_instance)
-		item_instance.collected.connect(update_score)
-		# item_instance.collected.connect($Player.update_score)
-
-	# TODO: need to make the world zoom out to this camera ready_up_camera()
+	reset()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+
 func update_score():
 	score += 1
 	$UI.update_score(score)
+
+
+func reset():
+	for n in 10:
+		var item_position = Vector2(
+			randf_range(0, tilemap_size.x * tilemap_scale.x),
+			randf_range(0, tilemap_size.y * tilemap_scale.y)
+		)
+		var item_instance = item_scene.instantiate()
+		item_instance.position = item_position
+		add_child(item_instance)
+		item_instance.collected.connect(update_score)
+
+	pan_entire_world()
--- player.gd
+++ player.gd
@@ -6,14 +6,9 @@ signal hit
 @export var speed = 400
 @export var sprint_mod = 2
 var screen_size
+var keys_disabled = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func process_keys(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
@@ -28,7 +23,7 @@ func _process(delta):
 		velocity = velocity.normalized() * speed
 		if Input.is_action_pressed("sprint"):
 			velocity *= sprint_mod
-		$AnimatedSprite2D.play("idle")
+			$AnimatedSprite2D.play("idle")
 	else:
 		$AnimatedSprite2D.stop()
 
@@ -36,12 +31,26 @@ func _process(delta):
 	# TODO: We need to correctly obtain the world boundary. Likely need to do it via the tilemap or something
 	# position = position.clamp(Vector2.ZERO, screen_size)
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if !keys_disabled:
+		process_keys(delta)
+
 
 func _on_body_entered(_body: Node2D):
 	# TODO: Probably should flash because damaged
 	hit.emit()
 
+func reset_camera_view():
+	$Camera2D.make_current()
+
 func reset(pos):
+	reset_camera_view()
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
